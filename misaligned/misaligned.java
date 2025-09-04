3b397080509347b87acaadb49b07fd9d79d6f8f6
YaminiSahu96 <58422405+YaminiSahu96@users.noreply.github.com>
2025-09-04 15:40:36 +0530

    Update misaligned.java

--- misaligned/misaligned.java
+++ misaligned/misaligned.java
@@ -12,7 +12,7 @@ public class Misaligned {
     }
     public static void main(String[] args) { 
         int result = printColorMap();
-        assert(result == 25);
+        assert(result == 36);
         System.out.println("All is well (maybe!)");
     }
 }
